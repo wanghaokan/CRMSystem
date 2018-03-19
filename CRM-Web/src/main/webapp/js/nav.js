7e5812ef825e7e9bccda915fe7bb59330d79c8ae
wanghaokan <778777101@qq.com>
2018-03-19 17:28:29 +0800

    3/19提交项目

---
+++ CRM-Web/src/main/webapp/js/nav.js
@@ -0,0 +1,247 @@
+var navs = [{
+	"title" : "后台首页",
+	"icon" : "icon-computer",
+	"href" : "page/main.html",
+	"spread" : false
+},{
+	"title" : "客户管理",
+	"icon" : "&#xe61c;",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "检测客户",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "我的客户",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "客户联系人",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "客户关怀",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "客户监控",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "客户转移",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+},{
+	"title" : "库存管理",
+	"icon" : "icon-text",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "库存列表",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "入库单",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "出库单",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "库存初始化",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+},{
+	"title" : "合同订单",
+	"icon" : "&#xe61c;",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "合同订单",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "交付订单",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "交付记录",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "交付计划",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "回款计划",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "回款记录",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},{
+			"title" : "开票记录",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+},{
+	"title" : "系统管理",
+	"icon" : "icon-text",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "职位管理",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "角色管理",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "用户管理",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "待办事项设定",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "日志管理",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+},{
+	"title" : "采购管理",
+	"icon" : "&#xe61c;",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "采购订单",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+},{
+	"title" : "供应商管理",
+	"icon" : "icon-text",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "供应商管理",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "供应商联系人",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+},{
+	"title" : "财务管理",
+	"icon" : "&#xe61c;",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "应收款",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "应付款",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "已收款",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "已付款",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "费用报销",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+},{
+	"title" : "客户跟踪",
+	"icon" : "icon-text",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "待办事宜",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "交往记录",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "跟踪监控",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+}]
---
+++ src/main/java/org/aptech/shiro/permission/CRMSystem/App.java
@@ -0,0 +1,13 @@
+package org.aptech.shiro.permission.CRMSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
